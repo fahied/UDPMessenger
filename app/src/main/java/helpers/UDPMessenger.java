e3272ce057ae1bb0c01668ed2e80298303db54e6
Muhammad Fahied <fahied@gmail.com>
2014-09-05 20:23:45 +0200

    extend with Runnable to allow Threads

--- app/src/main/java/helpers/UDPMessenger.java
+++ app/src/main/java/helpers/UDPMessenger.java
@@ -1,6 +1,12 @@
 package helpers;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.StrictMode;
+import android.preference.PreferenceManager;
+
+import com.spider.udpmessenger.R;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -9,33 +15,76 @@ import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 
 /**
  * Created by Spider on 02-Sep-14.
  */
-public class UDPMessenger {
+public class UDPMessenger implements Runnable
+{
     private DatagramSocket datagramSocket;
+    private DatagramPacket datagramPacket;
 
-    public void startUDPMessenger() throws SocketException {
-        //Allow to run Network ops on main thread just for trail mode
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        datagramSocket = new DatagramSocket();
-    }
+    private SharedPreferences sharedPreferences;
+    private Context context;
+
+    private String messageSequence;
+    private boolean shouldBroadcast;
+    private int destinationPort;
+    private String destinationIP;
+
     public void stopUDPMessenger() throws SocketException {
         //Close the port!
         datagramSocket.close();
     }
 
-    public void sendUDPMessage(String messageStr, int destinationPort,String destinationIP) throws IOException {
+
+    public UDPMessenger(Context context, String messageSequence, boolean shouldBroadcast) throws InterruptedException, IOException
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        datagramSocket = new DatagramSocket();
+
+        this.context = context;
+        this.messageSequence = messageSequence;
+        this.shouldBroadcast = shouldBroadcast;
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        loadPref();
+    }
+
+
+    private void loadPref() {
+        // broad to all preference
+        boolean checkbox_preference = sharedPreferences.getBoolean("checkbox_preference", false);
+        shouldBroadcast = checkbox_preference;
+
+        //Destination IP preference
+        destinationIP   = sharedPreferences.getString(context.getString(R.string.key_destination_ip),"127.0.0.1");
+        String port = sharedPreferences.getString(context.getString(R.string.key_port_number), "20002");
+        destinationPort = Integer.parseInt(port);
+
+    }
+
+
+    public void sendUDPMessage(String messageStr, int destinationPort,String destinationIP) {
 
         //datagramSocket.setBroadcast(false);
-        InetAddress local = InetAddress.getByName(destinationIP);
+        InetAddress local = null;
+        try {
+            local = InetAddress.getByName(destinationIP);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
         int msg_length=messageStr.length();
         byte[] message = messageStr.getBytes();
-        DatagramPacket p = new DatagramPacket(message, msg_length,local,destinationPort);
-        datagramSocket.send(p);
+        datagramPacket = new DatagramPacket(message, msg_length,local,destinationPort);
+        try {
+            datagramSocket.send(datagramPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(getClass().getName() + ">>> Request packet sent to: "+destinationIP +":"+destinationPort);
     }
 
@@ -66,8 +115,8 @@ public class UDPMessenger {
 
                     // Send the broadcast package!
                     try {
-                        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcast, destinationPort);
-                        datagramSocket.send(sendPacket);
+                        datagramPacket = new DatagramPacket(sendData, sendData.length, broadcast, destinationPort);
+                        datagramSocket.send(datagramPacket);
                     } catch (Exception e) {
                         System.out.println(e);
                     }
@@ -75,18 +124,64 @@ public class UDPMessenger {
                     System.out.println(getClass().getName() + ">>> Request packet sent to: " + broadcast.getHostAddress() + "; Interface: " + networkInterface.getDisplayName());
                 }
             }
-
-            //System.out.println(getClass().getName() + ">>> Done looping over all network interfaces. Now waiting for a reply!");
-
-            //Wait for a response
-            //byte[] recvBuf = new byte[15000];
-            //DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-            //c.receive(receivePacket);
-
-            //We have a response
-            //System.out.println(getClass().getName() + ">>> Broadcast response from server: " + receivePacket.getAddress().getHostAddress());
         } catch (IOException ex) {
             System.out.println(ex);
         }
+    }
+
+    @Override
+    public void run() {
+        if (this.messageSequence.equalsIgnoreCase(""))
+        {
+           // showErrorDialog("Empty Sequence","Go to settings and define message sequence");
+            return;
+        }
+        String[]parts = this.messageSequence.split("\n");
+        for (int i = 0; i < parts.length;i++)
+        {
+            String part = parts[i];
+            //if the message part is numeric then probably it is delay otherwise a message to be sent
+            if (isNumeric(part))
+            {
+                int delay  = Integer.parseInt(parts[i]);
+                //the time can only be positive int
+                delay = Math.abs(delay);
+                try {
+                    Thread.currentThread().sleep(delay);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            else if (!part.equalsIgnoreCase(""))
+            {
+                System.out.println(part);
+                if (shouldBroadcast)
+                    broadcastUDPMessage(part,destinationPort);
+                else sendUDPMessage(part, destinationPort, destinationIP);
+            }
+            // if all messages has been sent
+            if (i == parts.length-1)
+            {
+                //
+                //showErrorDialog("Messages sent successfully",messageSequence);
+            }
+
+        }
+    }
+
+    public static boolean isNumeric(String str)
+    {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+
+    private void showErrorDialog(String dialogTitle,String errorString){
+        String okButtonString = "OK";
+        AlertDialog.Builder ad = new AlertDialog.Builder(context);
+        ad.setTitle(dialogTitle);
+        ad.setMessage(errorString);
+        ad.show();
+        return;
     }
 }
